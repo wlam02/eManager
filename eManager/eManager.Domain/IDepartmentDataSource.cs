e92a44c6c9b5d78cb58a1dba11b9e66758ec2daa
wlam02 <aviram.moran@gmail.com>
2017-01-30 11:29:17 -0600

    Changed title

---
+++ eManager/eManager.Domain/IDepartmentDataSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eManager.Domain
+{
+    public class IDepartmentDataSource
+    {
+        public IQueryable<Employee> Employee { get; }
+        public IQueryable<Department> Department { get; }
+    }
+}
---
+++ eManager/eManager.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eManager.Web.Startup))]
+namespace eManager.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
